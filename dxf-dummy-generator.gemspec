e6f7113f5e67e78a64e0f3342ee7edc6019f45eb
Sille Van Landschoot <info@sillevl.be>
2013-06-20 09:49:13 +0200

    updated gem and added executable. Refactored class for integration with executable.

--- dxf-dummy-generator.gemspec
+++ dxf-dummy-generator.gemspec
@@ -1,6 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'dxf_dummy_generator'
-  s.version     = '0.0.1'
+  s.version     = '0.0.2'
+  s.executables << 'dxf-dummy-generator'
   s.date        = '2013-06-19'
   s.summary     = "DXF dummy generatore"
   s.description = "Creates dummy dxf files"
@@ -8,5 +9,5 @@ Gem::Specification.new do |s|
   s.email       = 'info@sillevl.be'
   s.files       = ["lib/dxf-dummy-generator.rb"] + Dir['lib/**/*.rb']
   s.homepage    =
-    'http://rubygems.org/gems/dxf-dummy-generator'
+    'http://rubygems.org/gems/dxf_dummy_generator'
 end
--- lib/dxf-dummy-generator.rb
+++ lib/dxf-dummy-generator.rb
@@ -3,34 +3,42 @@ require_relative 'dxf-dummy-generator/DxfOptions'
 require_relative 'dxf-dummy-generator/Dxf'
 
 
-options = DxfOptions.parse(ARGV)
-filename = Time.now.strftime("%Y%m%d%H%M%S").to_s
-filename += "_" + options[:dxf].lines.count.to_s
-filename += "r" + options[:dxf].lines.seed.to_s if options[:dxf].lines.random
-filename += "_" + options[:dxf].groups.count.to_s
-filename += "r" + options[:dxf].groups.seed.to_s if options[:dxf].groups.random
-filename += "_" + options[:dxf].contours.count.to_s
-filename += "r" + options[:dxf].contours.seed.to_s if options[:dxf].contours.random
-filename += ".dxf"
-filename = options[:filename].to_s unless options[:filename] == "default.dxf"
-dxf = DXF.new(options[:dxf])
+class DxfDummyGenerator
+	def initialize(args)
+		@options = DxfOptions.parse(args)
+		filename = getFileName(@options)
+		dxf = DXF.new(@options[:dxf])
+		printOutput(@options, filename)
+		saveFile(dxf, filename)
+	end
 
-if options[:verbose]
-	puts "Seed for random line generator: " + options[:dxf].lines.seed.to_s if options[:dxf].lines.count > 1 && options[:dxf].lines.random
-	puts "Seed for random group generator: " + options[:dxf].groups.seed.to_s if options[:dxf].groups.count > 1 && options[:dxf].groups.random
-	puts "Seed for random contour generator: " + options[:dxf].contours.seed.to_s if options[:dxf].contours.count > 1 && options[:dxf].contours.random
-	puts "Creating dxf file with " + options[:dxf].lines.count.to_s + " Lines"
-	puts "Writing to file " + filename
+	private
+	def getFileName(options)
+		filename = Time.now.strftime("%Y%m%d%H%M%S").to_s
+		filename += "_" + options[:dxf].lines.count.to_s
+		filename += "r" + options[:dxf].lines.seed.to_s if options[:dxf].lines.random
+		filename += "_" + options[:dxf].groups.count.to_s
+		filename += "r" + options[:dxf].groups.seed.to_s if options[:dxf].groups.random
+		filename += "_" + options[:dxf].contours.count.to_s
+		filename += "r" + options[:dxf].contours.seed.to_s if options[:dxf].contours.random
+		filename += ".dxf"
+		filename = options[:filename].to_s unless options[:filename] == "default.dxf"
+		filename
+	end
+
+	def printOutput(options, filename)
+		if options[:verbose]
+			puts "Seed for random line generator: " + options[:dxf].lines.seed.to_s if options[:dxf].lines.count > 1 && options[:dxf].lines.random
+			puts "Seed for random group generator: " + options[:dxf].groups.seed.to_s if options[:dxf].groups.count > 1 && options[:dxf].groups.random
+			puts "Seed for random contour generator: " + options[:dxf].contours.seed.to_s if options[:dxf].contours.count > 1 && options[:dxf].contours.random
+			puts "Creating dxf file with " + options[:dxf].lines.count.to_s + " Lines"
+			puts "Writing to file " + filename
+		end
+	end
+
+	def saveFile(dxf, filename)
+		File.open(filename, 'w') { |file| file.write(dxf.to_s) }
+	end
 end
-
-
-
-#File.open(filename, 'w') { |file| file.write(dxf.to_s) }
-
-#p options
-
-
-
-
 
 
